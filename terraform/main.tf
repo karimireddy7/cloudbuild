67a5ac4c9c3ffb480fdba2fad1ff4cc151fa621d
karimireddy7 <karimireddy7674@gmail.com>
2025-12-31 10:10:02 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,11 @@
-#
+terraform {
+  backend "gcs" {
+    bucket = "ck7674"
+    prefix = "terraform/tfstate"
+  }
+}
+resource "google_storage_bucket" "bucket-enabled" {
+  name          = "lumen-ck7674-bucket"
+  location      = "EU"
+  force_destroy = true
+}
