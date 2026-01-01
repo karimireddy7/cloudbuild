3cbd46c97e3bc091fb14246868835411ed86027d
karimireddy7 <karimireddy7674@gmail.com>
2026-01-01 10:16:46 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,26 +5,26 @@ terraform {
   }
 }
 resource "google_storage_bucket" "bucket-enabled" {
-  name          = "lumen-ck7674-bucket3"
+  name          = "lumen-ck7674-bucket1"
   project       = "ck7674"
   location      = "EU"
   force_destroy = true
 }
 ##
 resource "google_storage_bucket" "bucket-enabled1" {
-  name          = "lumen-ck7674-bucket4"
+  name          = "lumen-ck7674-bucket2"
   project       = "ck7674"
   location      = "EU"
   force_destroy = true
 }
 ##
 resource "google_compute_network" "vpc_network" {
-  name = "demo-vpc1"
+  name = "demo-vpc"
   project = "ck7674"
 }
 
 resource "google_compute_instance" "vm_instance" {
-   name          = "demo-vm2"
+   name          = "demo-vm"
    project      = "ck7674"
    machine_type = "e2-micro"
    zone          = "us-central1-a"
